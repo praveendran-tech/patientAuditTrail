9f1be56f0886ad8eceb2f8a9cdbdd19ca055f21f
Sumiran Jaiswal <sumi0309@umd.edu>
2025-01-25 18:37:24 -0500

    Add CORS policy for end to end communication

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ dotenv.config();
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "https://patient-audit-trail.vercel.app/", // Replace with your actual frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
+    credentials: true, // If you need to allow cookies
+  })
+);
+
 // Define allowed origins
 const allowedOrigins = ["http://localhost:3000", "http://localhost:5001"]; // Update as per your frontend's URLs
 
@@ -50,5 +59,5 @@ app.get("/", (req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
